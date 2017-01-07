319dd3ed1471b0c44b838b03d847baa1a77d7f58
Andrew Chen <Andrew-MBP@Andrews-MacBook-Pro.local>
2017-01-07 16:18:28 -0500

    added todos border plate

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,19 +1,30 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
-// Import needed templates
-import '../../ui/layouts/body/body.js';
-import '../../ui/pages/home/home.js';
-import '../../ui/pages/not-found/not-found.js';
+// Import to load these templates
+import '../../ui/layouts/app-body.js';
+import '../../ui/pages/root-redirector.js';
+import '../../ui/pages/lists-show-page.js';
+import '../../ui/pages/app-not-found.js';
 
-// Set up all routes in the app
-FlowRouter.route('/', {
-  name: 'App.home',
+// Import to override accounts templates
+import '../../ui/accounts/accounts-templates.js';
+
+FlowRouter.route('/lists/:_id', {
+  name: 'Lists.show',
   action() {
-    BlazeLayout.render('App_body', { main: 'App_home' });
+    BlazeLayout.render('App_body', { main: 'Lists_show_page' });
   },
 });
 
+FlowRouter.route('/', {
+  name: 'App.home',
+  action() {
+    BlazeLayout.render('App_body', { main: 'app_rootRedirector' });
+  },
+});
+
+// the App_notFound template is used for unknown routes and missing lists
 FlowRouter.notFound = {
   action() {
     BlazeLayout.render('App_body', { main: 'App_notFound' });
--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -1,4 +1,4 @@
-// Register your apis here
-
-import '../../api/links/methods.js';
-import '../../api/links/server/publications.js';
+import '../../api/lists/methods.js';
+import '../../api/lists/server/publications.js';
+import '../../api/todos/methods.js';
+import '../../api/todos/server/publications.js';
