77de4bd70a20c008eff58268b1abc89e7f1196e0
Syed Aman <sna274@nyu.edu>
2017-01-07 19:25:40 -0500

    update head and main

--- client/main.js
+++ client/main.js
@@ -1,4 +1,9 @@
 // Client entry point, imports all client code
 
-import '/imports/startup/client';
-import '/imports/startup/both';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+// import { renderRoutes } from '../imports/startup/client/routes.jsx';
+
+Meteor.startup(() => {
+  render(renderRoutes(), document.getElementById('app'));
+});
