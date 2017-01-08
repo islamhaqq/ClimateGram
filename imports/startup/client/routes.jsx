9218c210fae810737de4f5ca172c8568aa995f4a
Syed Aman <sna274@nyu.edu>
2017-01-08 05:04:53 -0500

    move wild-card path to the end

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -4,16 +4,15 @@ import { render } from 'react-dom';
 
 // import components
 import PageNotFound from '../../ui/pages/not-found/PageNotFound.jsx';
-// import MainLayout from '../../ui/layouts/MainLayout.jsx';
-// import GridListExampleSimple from '../../ui/component/GridListExampleSimple.jsx';
+import MainLayout from '../../ui/layouts/MainLayout.jsx';
+import GridListExampleSimple from '../../ui/component/GridListExampleSimple.jsx';
 
 // Set up all routes in the app
 export const renderRoutes = () => (
   <Router history={browserHistory}>
-    {/* <Route path="/" components={MainLayout}> */}
-    <Route>
+    <Route path="/" components={MainLayout}>
+      <Route path="main" components={GridListExampleSimple} />
       <Route path="*" component={PageNotFound}/>
-      {/* <Route path="main" components={GridListExampleSimple} /> */}
     </Route>
   </Router>
 );
