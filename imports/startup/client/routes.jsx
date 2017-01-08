eee7aec03471b0462019c718b50d3c2fb87fad20
Syed Aman <sna274@nyu.edu>
2017-01-07 20:44:22 -0500

    define routes

---
+++ imports/startup/client/routes.jsx
@@ -0,0 +1,13 @@
+// import { FlowRouter } from 'meteor/kadira:flow-router';
+// import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+
+import { React } from 'react';
+import { Router, Route, browserHistory } from 'react-router';
+
+// Import needed templates
+// import '../../ui/layouts/body/body.js';
+// import '../../ui/pages/home/home.js';
+// import '../../ui/pages/not-found/not-found.js';
+
+// Set up all routes in the app
+export const renderRoutes = () => ()
